4ab34bc27199bd34cdff292840cfb2a3aadff5ab
Michael Gearhardt <michael.gearhardt@gmail.com>
2021-08-10 08:50:54 -0600

    Expand select with some basic starting imply

--- src/controls/ExpandSelect.ts
+++ src/controls/ExpandSelect.ts
@@ -1,16 +1,18 @@
-
 // import { SJSElement } from '@semanticjs/common';
 
-export class ExpandSelect { //extends SJSElement {
+export class ExpandSelect {
+    //extends SJSElement {
     //  Fields
     protected mountedEl?: HTMLElement;
 
     //  Properties
     public Expanded?: boolean;
 
-    public Options?: Array<HTMLElement>;
+    public Options: Array<HTMLElement>;
 
     public OptionSelector?: string;
+
+    public SelectedIndex: number;
 
     //  Constructors
     constructor() {
@@ -19,9 +21,11 @@ export class ExpandSelect { //extends SJSElement {
         this.Options = [];
 
         this.OptionSelector = '.option';
+
+        this.SelectedIndex = 0;
     }
 
-    //  API Methods
+    //  Life Cycle
     public Mount(selector: string | HTMLElement): void {
         if (typeof selector === 'string') {
             selector = <HTMLElement>document.querySelector(<string>selector);
@@ -30,12 +34,61 @@ export class ExpandSelect { //extends SJSElement {
         this.mountedEl = this.mountToElement(<HTMLElement>selector);
     }
 
+    //  API Methods
+    public SetSelectedIndex(index: number | HTMLElement): void {
+        if (index instanceof HTMLElement) {
+            index = this.Options.indexOf(index);
+            debugger;
+        }
+
+        this.SelectedIndex = index;
+        debugger;
+
+        this.setupActiveElement();
+    }
+
     //  Helpers
     protected mountToElement(el: HTMLElement): HTMLElement {
-        const optionElements = el.querySelectorAll(<string>this.OptionSelector);
-        
-        optionElements.forEach(oe => oe.classList.add('testing'));
+        this.setupOptions(el);
+
+        this.setupActiveElement(el);
+
+        this.setupActiveElement(el);
 
         return el;
+    }
+
+    protected removeAllChildren(el?: HTMLElement): void {
+        const workingEl = el || this.mountedEl;
+
+        workingEl?.childNodes?.forEach((cn) => {
+            workingEl?.removeChild(cn);
+        });
+    }
+
+    protected setupOptions(el?: HTMLElement): void {
+        const workingEl = el || this.mountedEl;
+
+        const optionElements = workingEl?.querySelectorAll(
+            <string>this.OptionSelector
+        );
+
+        optionElements?.forEach((oe) => {
+            oe?.addEventListener('click', (ev) => {
+                this.SetSelectedIndex(<HTMLElement>ev.currentTarget);
+            });
+
+            this.Options.push(<HTMLElement>oe);
+
+            workingEl?.removeChild(oe);
+        });
+    }
+
+    protected setupActiveElement(el?: HTMLElement): void {
+        const workingEl = el || this.mountedEl;
+
+        this.removeAllChildren(workingEl);
+
+        workingEl?.appendChild(this.Options[this.SelectedIndex]);
     }
 }
