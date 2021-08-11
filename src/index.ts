cbd76bde830164e244d20e04707e3d2be6f509c3
xchanin <shannonwbruns@gmail.com>
2021-08-11 09:26:21 -0600

    removed test control

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { PublicWebApplication } from './app/PublicWebApplication.js'
 import { HTMLHead } from './common/HTMLHead.js'
-import { TesterTest } from './controls/TesterTest.js';
 import { ExpandSelect } from './controls/ExpandSelect.js';
 import { Carousel } from './controls/Carousel.js';
 
-export { PublicWebApplication, HTMLHead, TesterTest, ExpandSelect, Carousel };
+export { PublicWebApplication, HTMLHead, ExpandSelect, Carousel };
