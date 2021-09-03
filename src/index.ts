73bd2609dd7bfa49dcab01a7b3c32ac36462ab1e
Michael Gearhardt <michael.gearhardt@gmail.com>
2021-09-03 14:36:40 -0600

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import { ExpandSelect } from './controls/ExpandSelect.js';
 import { Carousel } from './controls/Carousel.js';
 import { CarouselTemplate } from './templates/CarouselTemplate.js';
 import { ComponentTest } from './components/loaderTest/component.js';
+import { LazyImage } from './controls/LazyImage.js'
 
-export { PublicWebApplication, HTMLHead, ExpandSelect, Carousel, CarouselTemplate, ComponentTest };
+export { PublicWebApplication, HTMLHead, ExpandSelect, Carousel, CarouselTemplate, ComponentTest, LazyImage };
