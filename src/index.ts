09f7823428333381eb83b72a694465f32dfe80a4
Michael Gearhardt <michael.gearhardt@gmail.com>
2021-09-03 16:40:40 -0600

    Format

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { PublicWebApplication } from './app/PublicWebApplication.js'
-import { HTMLHead } from './common/HTMLHead.js'
+import { PublicWebApplication } from './app/PublicWebApplication.js';
+import { HTMLHead } from './common/HTMLHead.js';
 import { ExpandSelect } from './controls/ExpandSelect.js';
 import { Carousel } from './controls/Carousel.js';
 import { CarouselTemplate } from './templates/CarouselTemplate.js';
 import { ComponentTest } from './components/loaderTest/component.js';
-import { LazySource } from './controls/LazySource.js'
+import { LazySource } from './controls/LazySource.js';
 
-export { PublicWebApplication, HTMLHead, ExpandSelect, Carousel, CarouselTemplate, ComponentTest, LazySource };
+export {
+  PublicWebApplication,
+  HTMLHead,
+  ExpandSelect,
+  Carousel,
+  CarouselTemplate,
+  ComponentTest,
+  LazySource,
+};
